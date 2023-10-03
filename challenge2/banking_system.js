b19e1d91bd09d9fb48aee64fd3ffe8e317ef34ae
Fajar Dwi Utomo <99060119+fajarutamaa@users.noreply.github.com>
2023-10-03 11:12:06 +0700

    Create banking_system.js

---
+++ challenge2/banking_system.js
@@ -0,0 +1,22 @@
+class BankAcount {
+    constructor(saldo) {
+        this.saldo = 0
+    }
+
+    deposit(amount) {
+        let = amount(parseFloat(prompt('Masukkan jumlah saldo')))
+        try {
+            if (amount(!isNaN)) {
+
+            }
+        } catch {
+
+        }
+
+
+    }
+
+    withdraw(amount) {
+        let = amount(parseFloat(prompt()))
+    }
+}
