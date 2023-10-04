4a977b86fd447187bc25ad21d40df9f4b62301cb
Fajar Dwi Utomo <fajardwiutomo75@gmail.com>
2023-10-04 13:37:06 +0700

    update file banking system

--- challenge2/banking_system.js
+++ challenge2/banking_system.js
@@ -1,22 +1,59 @@
-class BankAcount {
-    constructor(saldo) {
-        this.saldo = 0
+//initiali class BankAccount
+class BankAccount {
+    //Define constructor with paramater balance = 0
+    constructor(balance = 0) {
+        this.saldo = balance
     }
 
-    deposit(amount) {
-        let = amount(parseFloat(prompt('Masukkan jumlah saldo')))
+    //initializa method deposit
+    async deposit(amount) {
+        let amount = parseFloat(prompt('Masukkan jumlah saldo'))
         try {
-            if (amount(!isNaN)) {
-
+            if (!isNaN(amount) && amount > 0) {
+                this.saldo += amount
+                this.balance()
+                await this.showMessage(`Setor Tunai Sebesar ${this.saldo} Berhasil`)
+            } else {
+                throw new Error('process invalid')
             }
-        } catch {
-
         }
-
-
+        catch (err) {
+            console.error('catch error : ', err)
+        }
+        return this.saldo
     }
 
-    withdraw(amount) {
-        let = amount(parseFloat(prompt()))
+    //intializas method withdraw
+    async withdraw(amount) {
+        let amount = parseFloat(prompt('Masukkan jumlah saldo'))
+        try {
+            if (!isNan(amount) && jumlah > 0 && jumlah <= this.saldo) {
+                this.saldo -= amount
+                this.balance()
+                await this.showMessage(`Tarik Tunai Sebesar ${this.saldo} Berhasil`)
+            } else {
+                throw new Error('process invalid')
+            }
+        } catch (err) {
+            console.error('catch error : ', err)
+        }
+        return this.saldo
+    }
+
+    async showMessage(msg) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                alert(msg)
+                resolve('success')
+            }, 1000)
+        })
+    }
+
+
+    //intializas conditional balance
+    balance() {
+        return this.saldo
     }
 }
+
+const bankAccount = new BankAccount()
