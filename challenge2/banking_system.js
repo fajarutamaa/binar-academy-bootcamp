4646779ce787df80c610790370f1d1b2a0e676b2
Fajar Dwi Utomo <fajardwiutomo75@gmail.com>
2023-10-05 10:52:01 +0700

    add index.html and update banking system

--- challenge2/banking_system.js
+++ challenge2/banking_system.js
@@ -1,15 +1,15 @@
-//itializeclass BankAccount
+//iinitialize class BankAccount
 class BankAccount {
     //Define constructor with property saldo
     constructor(balance = 0) {
         this.saldo = balance
     }
 
-    //nitialize method deposit
-    async deposit(amount) {
+    //initialize method deposit
+    async deposit() {
         let amount = parseFloat(prompt('Masukkan jumlah saldo'))
         try {
-            if (!isNaN(amount) && amount > 0) {
+            if (amount != NaN && amount > 0) {
                 this.saldo += amount
                 this.showBalance()
                 await this.showMessage(`Setor Tunai Sebesar ${this.saldo} Berhasil`)
@@ -18,16 +18,16 @@ class BankAccount {
             }
         }
         catch (err) {
-            console.error('catch error : ', err)
+            console.error('Message error : ', err.message)
         }
         return this.saldo
     }
 
     //initialize method withdraw
-    async withdraw(amount) {
+    async withdraw() {
         let amount = parseFloat(prompt('Masukkan jumlah saldo'))
         try {
-            if (!isNan(amount) && amount > 0 && amount <= this.saldo) {
+            if (amount != NaN && amount > 0 && amount <= this.saldo) {
                 this.saldo -= amount
                 this.showBalance()
                 await this.showMessage(`Tarik Tunai Sebesar ${this.saldo} Berhasil`)
@@ -35,20 +35,20 @@ class BankAccount {
                 throw new Error('process invalid')
             }
         } catch (err) {
-            console.error('catch error : ', err)
+            console.error('Message error : ', err.message)
         }
         return this.saldo
     }
 
+    //initialize conditional set time out at process transactions
     async showMessage(msg) {
         return new Promise((resolve) => {
             setTimeout(() => {
                 alert(msg)
                 resolve('success')
-            }, 1000)
+            }, 5000)
         })
     }
-
 
     //initialize conditional balance
     showBalance() {
